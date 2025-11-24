f22a86519e1a86a787265b80ef82839abe75ba12
Andrey Kolodyazhnyy <andrey.kolodyazhnyy@simpleone.ru>
2025-11-24 13:36:15 +0300

    fix: update eslint to v9 config format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,13 @@
-// import { defineConfig, globalIgnores } from 'eslint/config';
-// import nextVitals from 'eslint-config-next/core-web-vitals';
-// import nextTs from 'eslint-config-next/typescript';
-
-// const eslintConfig = defineConfig([
-//   ...nextVitals,
-//   ...nextTs,
-//   // Override default ignores of eslint-config-next.
-//   globalIgnores([
-//     // Default ignores of eslint-config-next:
-//     '.next/**',
-//     'out/**',
-//     'build/**',
-//     'next-env.d.ts',
-//   ]),
-// ]);
-
-// export default eslintConfig;
-
 import next from 'eslint-config-next';
-import prettier from 'eslint-config-prettier';
 
 export default [
   ...next,
-  prettier,
   {
+    files: ['**/*.ts', '**/*.tsx'],
     rules: {
-      'prefer-const': 'warn',
+      // ВСЕ правила как предупреждения или отключены
       'no-unused-vars': 'warn',
+      'prefer-const': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
